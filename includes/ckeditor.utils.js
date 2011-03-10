32f665f938299afadd00a33b0445af7befd0f09c
Damian Czepierga <damian@czepierga.pl>
2011-03-10 11:59:48 +0100

    Add copyright header to ckeditor.utils.js

--- includes/ckeditor.utils.js
+++ includes/ckeditor.utils.js
@@ -1,3 +1,32 @@
+/*
+ * CKEditor - The text editor for the Internet - http://ckeditor.com
+ * Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+ *
+ * == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your
+ * choice:
+ *
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ *    http://www.gnu.org/licenses/lgpl.html
+ *
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * == END LICENSE ==
+ *
+ * @file
+ * CKEditor Module for Drupal 7.x
+ *
+ * This module allows Drupal to replace textarea fields with CKEditor.
+ *
+ * This HTML text editor brings to the web many of the powerful functionalities
+ * of known desktop editors like Word. It's really  lightweight and doesn't
+ * require any kind of installation on the client computer.
+ */
 var CKEDITOR_BASEPATH = Drupal.settings.ckeditor.editor_path;
 (function ($) {
     Drupal.ckeditor = (typeof(CKEDITOR) != 'undefined');
